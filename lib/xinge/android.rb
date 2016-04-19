e1616f4a31f992e9a56ce70fcc06b9c9caa9062d
yuzixun <yzxun@qq.com>
2016-04-19 14:54:44 +0800

    add push to tag method for ios and android

--- lib/xinge/android.rb
+++ lib/xinge/android.rb
@@ -11,6 +11,10 @@ module Xinge
       self.push_all_device(1, build_simple_message(title, content, custom_content), params)
     end
 
+    def pushTagsDevice(title, content, tags_list, tags_op = "AND", custom_content = {})
+      self.push_tags_device(1, build_simple_message(title, content, custom_content), tag_list, tags_op, params.merge({environment: ENV_MAP[Xinge.config[:env]]}))
+    end
+
     protected
 
     def build_simple_message(title, content, custom_content)
